bcbd389aedb3b9e5f64e7e68fb08a5ba194c21f9
Samuel <samuel.alvessd@gmail.com>
2023-11-13 21:46:17 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/tarefa/Login";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
